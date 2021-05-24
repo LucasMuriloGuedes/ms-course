3812415473d27e900e3ce82f44e92da2436e94cb
LucasMuriloGuedes <lucasmurilo.guedes@gmail.com>
2021-05-24 13:23:57 -0400

    Project hr - worked create

---
+++ src/main/java/com/lucasmurilo/hrworkwe/HrWorkweApplication.java
@@ -0,0 +1,13 @@
+package com.lucasmurilo.hrworkwe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrWorkweApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrWorkweApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucasmurilo/hrworkwe/HrWorkweApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasmurilo.hrworkwe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkweApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
